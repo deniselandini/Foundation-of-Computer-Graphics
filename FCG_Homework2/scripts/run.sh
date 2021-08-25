6f7ae717aa34a23aa17b6c6838d6395eeaacd5c6
Denise Landini <79659258+deniselandini@users.noreply.github.com>
2021-08-25 23:09:36 +0200

    Add files via upload

---
+++ FCG_Homework2/scripts/run.sh
@@ -0,0 +1,86 @@
+./bin/ypathtrace tests/01_cornellbox/cornellbox.json -o out/naive/01_cornellbox_512_256.jpg -t naive -s 256 -r 512
+./bin/ypathtrace tests/02_matte/matte.json -o out/naive/02_matte_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/03_metal/metal.json -o out/naive/03_metal_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/04_plastic/plastic.json -o out/naive/04_plastic_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/05_glass/glass.json -o out/naive/05_glass_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/06_opacity/opacity.json -o out/naive/06_opacity_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/07_hair/hair.json -o out/naive/07_hair_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/08_lens/lens.json -o out/naive/08_lens_720_256.jpg -t naive -s 256 -r 720
+
+./bin/ypathtrace tests/01_cornellbox/cornellbox.json -o out/path/01_cornellbox_512_256.jpg -t path -s 256 -r 512
+./bin/ypathtrace tests/02_matte/matte.json -o out/path/02_matte_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/03_metal/metal.json -o out/path/03_metal_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/04_plastic/plastic.json -o out/path/04_plastic_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/05_glass/glass.json -o out/path/05_glass_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/06_opacity/opacity.json -o out/path/06_opacity_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/07_hair/hair.json -o out/path/07_hair_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/08_lens/lens.json -o out/path/08_lens_720_256.jpg -t path -s 256 -r 720
+
+./bin/ypathtrace tests/11_bathroom1/bathroom1.json -o out/path/11_bathroom1_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/12_ecosys/ecosys.json -o out/path/12_ecosys_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/13_bedroom/bedroom.json -o out/path/13_bedroom_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/14_car1/car1.json -o out/path/14_car1_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/15_classroom/classroom.json -o out/path/15_classroom_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/16_coffee/coffee.json -o out/path/16_coffee_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/17_kitchen/kitchen.json -o out/path/17_kitchen_720_256.jpg -t path -s 256 -r 720
+
+# naive 
+./bin/ypathtrace tests/01_cornellbox/cornellbox.json -o out/naive/01_cornellbox_512_256.jpg -t naive -s 256 -r 512
+./bin/ypathtrace tests/02_matte/matte.json -o out/naive/02_matte_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/03_metal/metal.json -o out/naive/03_metal_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/04_plastic/plastic.json -o out/naive/04_plastic_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/05_glass/glass.json -o out/naive/05_glass_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/06_opacity/opacity.json -o out/naive/06_opacity_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/07_hair/hair.json -o out/naive/07_hair_720_256.jpg -t naive -s 256 -r 720
+./bin/ypathtrace tests/07_hair/hair.json -o out/naive/07_hair_1280_1024.jpg -t naive -s 1024 -r 1280 #this is for the comparison with the stratified_sampling
+./bin/ypathtrace tests/08_lens/lens.json -o out/naive/08_lens_720_256.jpg -t naive -s 256 -r 720
+
+#path
+./bin/ypathtrace tests/01_cornellbox/cornellbox.json -o out/path/01_cornellbox_512_256.jpg -t path -s 256 -r 512
+./bin/ypathtrace tests/02_matte/matte.json -o out/path/02_matte_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/03_metal/metal.json -o out/path/03_metal_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/04_plastic/plastic.json -o out/path/04_plastic_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/05_glass/glass.json -o out/path/05_glass_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/06_opacity/opacity.json -o out/path/06_opacity_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/07_hair/hair.json -o out/path/07_hair_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/08_lens/lens.json -o out/path/08_lens_720_256.jpg -t path -s 256 -r 720
+
+./bin/ypathtrace tests/11_bathroom1/bathroom1.json -o out/path/11_bathroom1_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/12_ecosys/ecosys.json -o out/path/12_ecosys_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/13_bedroom/bedroom.json -o out/path/13_bedroom_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/14_car1/car1.json -o out/path/14_car1_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/15_classroom/classroom.json -o out/path/15_classroom_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/16_coffee/coffee.json -o out/path/16_coffee_720_256.jpg -t path -s 256 -r 720
+./bin/ypathtrace tests/17_kitchen/kitchen.json -o out/path/17_kitchen_720_256.jpg -t path -s 256 -r 720
+
+# big scenes
+ ./bin/ypathtrace tests/extracredit_bistrointerior/bistrointerior.json -o out/path/extracredit_bistrointerior_1280_1024.jpg -t path -s 1024 -r 1280
+ ./bin/ypathtrace tests/extracredit_bistroexterior/bistroexterior.json -o out/path/extracredit_bistroexterior_1280_1024.jpg -t path -s 1024 -r 1280
+ ./bin/ypathtrace tests/extracredit_landscape/landscape.json -o out/path/extracredit_landscape_1280_1024.jpg -t path -s 1024 -r 1280
+ ./bin/ypathtrace tests/extracredit_rungholt/rungholt.json -o out/path/extracredit_rungholt_1280_1024.jpg -t path -s 1024 -r 1280
+ ./bin/ypathtrace tests/extracredit_sanmiguel/sanmiguel.json -o out/path/extracredit_sanmiguel_1280_1024.jpg -t path -s 1024 -r 1280
+ 
+ # stratified sampling 1
+  ./bin/ypathtrace tests/01_cornellbox/cornellbox.json -o out/stratified_sampling_1/01_cornellbox_512_256.jpg -t extra -s 256 -r 512
+  ./bin/ypathtrace tests/02_matte/matte.json -o out/stratified_sampling_1/02_matte_720_256.jpg -t extra -s 256 -r 720
+  ./bin/ypathtrace tests/06_opacity/opacity.json -o out/stratified_sampling_1/06_opacity_720_256.jpg -t extra -s 256 -r 720
+  ./bin/ypathtrace tests/07_hair/hair.json -o out/stratified_sampling_1/07_hair_720_256.jpg -t extra -s 1024 -r 1280
+  ./bin/ypathtrace tests/08_lens/lens.json -o out/stratified_sampling_1/08_lens_720_256.jpg -t extra -s 256 -r 720
+  ./bin/ypathtrace tests/12_ecosys/ecosys.json -o out/stratified_sampling_1/12_ecosys_720_256.jpg -t extra -s 256 -r 720
+
+ # I described my scene in the readme_1938388
+ # my scene 0 - Set table
+ ./bin/ypathtrace tests/scene_project0/scene0.json -o out/path/my_scene_0.jpg -t path -s 256 -r 720
+
+ # my scene 1 - Illuminated garden
+ ./bin/ypathtrace tests/scene_project1/scene1.json -o out/path/my_scene_1.jpg -t path -s 512 -r 720
+
+ # my scene 2 - Fire breathing dragon 
+  ./bin/ypathtrace tests/scene_project2/scene2.json -o out/path/my_scene_2.jpg -t path -s 512 -r 720
+
+ # my scene 3 - Combination of geometries
+  ./bin/ypathtrace tests/scene_project3/scene3.json -o out/path/my_scene_3.jpg -t path -s 256 -r 720
+
+ # my scene 4 - BMW with illuminated Christmas tree and gifts
+  ./bin/ypathtrace tests/scene_project4/scene4.json -o out/path/my_scene_4.jpg -t path -s 512 -r 720
+
---
+++ FCG_Homework2/scripts/sync_yocto.sh
@@ -0,0 +1,2 @@
+cp -r ~/Documents/Work/projects/yocto-gl/libs/yocto/* libs/yocto/
+cp -r ~/Documents/Work/projects/yocto-gl/libs/yocto_gui/* libs/yocto_gui/
